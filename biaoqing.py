22ef9d6ade33b3c653d4ff062f2a4092891857a5
kins <kins36936@gmail.com>
2018-08-10 14:48:25 +0800

    Fix biaoqing name issue

--- biaoqing.py
+++ biaoqing.py
@@ -5,17 +5,18 @@ import requests
 from bs4 import BeautifulSoup
 from multiprocessing import Pool, cpu_count
 
-
 HEADERS = {
-    'X-Requested-With': 'XMLHttpRequest',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 '
-                  '(KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
-    'Referer': "http://www.fabiaoqing.com"
+    'X-Requested-With':
+    'XMLHttpRequest',
+    'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 '
+    '(KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+    'Referer':
+    "http://www.fabiaoqing.com"
 }
 
+
 # 创建当日表情包文件夹
-
-
 def makedir():
     today = time.strftime("%Y%m%d")
     if not os.path.exists(today):
@@ -24,9 +25,8 @@ def makedir():
     else:
         os.chdir(today)
 
+
 # 保存表情
-
-
 def save_pic(realbq_url, bq_name):
     img = requests.get(realbq_url, headers=HEADERS, timeout=10)
     with open(bq_name, 'ab') as f:
@@ -37,16 +37,18 @@ def save_pic(realbq_url, bq_name):
 # 主爬虫，获取图片url，正则匹配图片格式后缀作为文件名后缀
 def crawler(url):
     r = requests.get(url, headers=HEADERS).text
-    img_infos = BeautifulSoup(r, 'lxml').find(
-        'div', {'class': 'ui segment imghover'}).find_all('a')
+    img_infos = BeautifulSoup(r, 'lxml').find('div', {
+        'class': 'ui segment imghover'
+    }).find_all('a')
     for img_info in img_infos:
         url_info = img_info.find('img', {'class': 'ui image lazy'})
         realbq_url = url_info['data-original']
         realbq_url = re.sub('bmiddle', 'large', realbq_url)
         bq_name = url_info['title']
-        suffix = re.search(
-            'jpg|bmp|gif|ico|pcx|jpeg|tif|png|raw|tga', realbq_url).group()
-        bq_name += '.'+suffix
+        bq_name = re.sub('。|:|-|\?', '', bq_name)
+        suffix = re.search('jpg|bmp|gif|ico|pcx|jpeg|tif|png|raw|tga',
+                           realbq_url).group()
+        bq_name += '.' + suffix
         save_pic(realbq_url, bq_name)
 
 
@@ -54,6 +56,7 @@ if __name__ == '__main__':
     makedir()
     bq_url = [
         'http://fabiaoqing.com/biaoqing/lists/page/{cnt}.html'.format(cnt=cnt)
-        for cnt in range(0, 6)]
+        for cnt in range(0, 6)
+    ]
     pool = Pool(processes=cpu_count())
     pool.map(crawler, bq_url)
